38c3d5ec3d28eed8d408aa96edac3974ec4a1c34
Филип Арсовски <arsovskidev@gmail.com>
2021-07-20 21:50:35 +0200

    Completed Day 1.

---
+++ day-1/learning/exercise-3.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age?\n")
+# 🚨 Don't change the code above 👆
+
+# Write your code below this line 👇
+
+years = 90 - int(age)
+days = years * 365
+weeks = years * 52
+months = years * 12
+
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
