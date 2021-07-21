83f66f2dca8ff73e14c90fb9e77483ff8bea4eaf
Филип Арсовски <arsovskidev@gmail.com>
2021-07-21 23:43:14 +0200

    Completed Day 2.

--- day-2/learning/exercise-5.py
+++ day-2/learning/exercise-5.py
@@ -6,8 +6,24 @@ name2 = input("What is their name? \n")
 
 # Write your code below this line 👇
 
-lower_case_name1 = name1.lower()
-lower_case_name2 = name2.lower()
+combined_names = name1.lower() + name2.lower()
 
+letter_t = combined_names.count("t")
+letter_r = combined_names.count("r")
+letter_u = combined_names.count("u")
+letter_e = combined_names.count("e")
+letter_l = combined_names.count("l")
+letter_o = combined_names.count("o")
+letter_v = combined_names.count("v")
 
-print(name1.count("f"))
+total_true = str(letter_t + letter_r + letter_u + letter_e)
+total_love = str(letter_l + letter_o + letter_v + letter_e)
+
+score = int(total_true + total_love)
+
+if (score < 10) or (score > 90):
+    print(f"Your score is {score}, you go together like coke and mentos.")
+elif (score >= 40) and (score <= 50):
+    print(f"Your score is {score}, you are alright together.")
+else:
+    print(f"Your score is {score}.")
