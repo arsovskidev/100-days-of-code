712c7f2839607f629b5ea5e81732032734fe60dd
Филип Арсовски <arsovskidev@gmail.com>
2021-07-24 23:22:39 +0200

    Completed Day 5.

--- day-5/project/main.py
+++ day-5/project/main.py
@@ -1,24 +1,20 @@
-# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
-
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 
 def turn_right():
     turn_left()
     turn_left()
     turn_left()
 
-
-def jump():
-    turn_left()
+while front_is_clear():
     move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
-
-
+    
+turn_left()
+    
 while not at_goal():
-    if wall_in_front():
-        jump()
-    else:
+    if right_is_clear():
+        turn_right()
         move()
+    elif front_is_clear():
+        move()
+    else:
+        turn_left()
