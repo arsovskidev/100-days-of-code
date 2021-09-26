844998e9c19e8b8c12ea8ee33d5ee1ab91675963
Филип Арсовски <arsovskidev@gmail.com>
2021-09-26 18:51:53 +0200

    Completed Day 21.

---
+++ day-021/learning/learning-1.py
@@ -0,0 +1,24 @@
+class Animal:
+    def __init__(self):
+        self.num_eyes = 2
+
+    def breath(self):
+        print("Inhale, exhale.")
+
+
+class Fish(Animal):
+    def __init__(self):
+        super().__init__()
+
+    def breath(self):
+        super().breath()
+        print("(doing this underwater.)")
+
+    def swim(self):
+        print("Moving in water.")
+
+
+nemo = Fish()
+nemo.swim()
+nemo.breath()
+print(nemo.num_eyes)
